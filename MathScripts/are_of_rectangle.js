879cfa2e62b4c6d666af8bf8a85ab33aae28f8a5
LeilaC1 <leilaclesca13@gmail.com>
2023-10-17 14:44:16 -0400

    did are of rect

---
+++ MathScripts/are_of_rectangle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// rectangle area = width * length
+
+var rectangleWidth = 4;
+var rectangleLength = 8;
+var rectangleArea = rectangleLength * rectangleWidth;
+
+// output
+console.log("The area of the rectangle is " + rectangleArea)
