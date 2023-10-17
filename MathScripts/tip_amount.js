f3a661dad8bc04c61218fc9e606c189e02c80ef7
LeilaC1 <leilaclesca13@gmail.com>
2023-10-17 15:01:02 -0400

    did tip amount

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// tip amount of a food bill given tip percentage
+
+var foodBill = 30;
+var tipPercentage = 20;
+
+var tipAmount = (foodBill * tipPercentage) / 100;
+// output
+
+console.log("The tip on a " + foodBill + " food bill is " + tipAmount);
