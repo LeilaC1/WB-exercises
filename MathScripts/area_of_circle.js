4c92a8603467ded573b25bf1cae43a728e4e6258
LeilaC1 <leilaclesca13@gmail.com>
2023-10-17 15:08:49 -0400

    did area of circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// A = pi * r^2
+
+var pi = 3.1415;
+var radius = 4;
+var circleArea = pi * radius **2;
+
+console.log("The area of a circle with radius " + radius + " is " + circleArea);
