d6dc329fbf11bccfabeea110a2f814bb343248bd
LeilaC1 <leilaclesca13@gmail.com>
2023-10-19 09:07:47 -0400

    did converrsions

---
+++ NumericCoversions/conversion_tests.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Description : This script test various numeric coversion techniques
+// Author: Jordan Q. Newprogrammer
+
+var a = +" 101.1 ";
+// var a = +a
+var b = +"55";
+var c = +"402 Stevens";
+var d = +"Number 5 ";
+
+console.log(a)
