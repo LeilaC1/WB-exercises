f58d0c44451f7b7da5661470932e48a6714ff817
LeilaC1 <leilaclesca13@gmail.com>
2023-10-19 21:04:36 -0400

    did base of complex tax-- need improvement

---
+++ IfScripts/min_max.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var a = 4;
+var b = 8;
+var c = 16;
+var smallestValue;
+var largestValue;
+
+
+
+
+// if (a < b && c) {
+// largestValue =  "a";  
+// } else if (b < a && c) {
+//     largestValue =  "b"; 
+// } else {
+//     largestValue = "c";
+// }
+
+
+// console.log("largest value = " + largestValue)
+
+// if (a < b && c) {
+ //   smallestValue =  "a";  
+  //   } else if (b < a && c) {
+   //      smallestValue =  "b"; 
+ //    } else if (c < a && b){
+   //      smallestValue =  "c";
+   //  }
